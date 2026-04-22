ba4c4f568b3b1ca28232aa60985e5e67b87f9e2c
Naveen Singh <naveenshubham620@gmail.com>
2026-04-22 10:30:45 +0530

    22april2k26

--- dsa.py
+++ dsa.py
@@ -72,16 +72,13 @@
 # print(selectionSort(arr))
 
 
-bubbleSort = [12 , 8 , 7 , 3 , 19 , 98 , 20  ]
-def bubbleSort(arr):
-    n = len(arr)
-    for i in range(n):
-        for j in range(0, n-i-1):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-    return arr
-print(bubbleSort(bubbleSort))
-
-
-
+# bubbleSort = [12 , 8 , 7 , 3 , 19 , 98 , 20  ]
+# def bubbleSort(arr):
+#     n = len(arr)
+#     for i in range(n):
+#         for j in range(0, n-i-1):
+#             if arr[j] > arr[j+1]:
+#                 arr[j], arr[j+1] = arr[j+1], arr[j]
+#     return arr
+# print(bubbleSort(bubbleSort))
 
