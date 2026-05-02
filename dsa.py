2d2edf6bb881b1636ff710894a43aa8d3e89f97d
Naveen Singh <naveenshubham620@gmail.com>
2026-05-02 23:00:32 +0530

    order list

--- dsa.py
+++ dsa.py
@@ -1,15 +1,4 @@
 # Data Structures 
-# class DoublyLinkedListNode:
-#     def __init__(self, value):
-#         self.value = value
-#         self.prev = None
-#         self.next = None
-
-# class DoublyLinkedList:
-#     def __init__(self):
-#         self.head = None
-#         self.tail = None
-
 #     def append(self, value):
 #         new_node = DoublyLinkedListNode(value)
 #         if not self.head:
@@ -81,4 +70,135 @@
 #                 arr[j], arr[j+1] = arr[j+1], arr[j]
 #     return arr
 # print(bubbleSort(bubbleSort))
+
+# matrix:
+
+# Toggle case function
+# def toggle_case(s):
+#     return ''.join(c.upper() if c.islower() else c.lower() for c in s)
+
+# print(toggle_case("world"))
+
+# from collection import defaultdict
+# def createAdjustmentList():
+#     n  = int(input("Enter the number of adjustments: "))
+#     adj_list = defaultdict(list)
+#     for _ in range(n):
+#         node = input("Enter the node: ")
+#         adj_list[node]
+
+#     edges = int(input("Enter the number of edges: "))
+
+#     isDirected = int(input("enter 1 for directed graph otherwise 0: "))
+
+#     for _ in range(edges):
+#         s = input("Enter the source node: ")
+#         d = input("Enter the destination node: ")
+#         adj_list[s] = d
+#         if isDirected == 0:
+#             adj_list[d] = s
+#     return adj_list
+
+# myAdjList = createAdjList()
+# print(myAdjList)
+  
+
+
+# # INORDER TRAVERSA
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# def inorder_recursive(root):
+#     result = []
+
+#     def traverse(node):
+#         if node is none:
+#             return
+        
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val=val
+#         self.left=left
+#         self.right=right
+
+# def inorder_recursive(root):
+#     result = []
+#     def traverse(node):
+#         if not node:
+#             return
+#         traverse(node.left)
+#         result.append(node.val)
+#         traverse(node.right)
+#     traverse(root)
+#     return result
+
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val=val
+        self.left=left
+        self.right=right
+
+def postorder_recursive(root):
+    result= []
+    def traverse(node):
+        if not node:
+            return
+        traverse(node.left)
+        result.append(node.val)
+        traverse(node.right)
+    traverse(root)
+    return result
+
+
+# def inorder_iterative(root):
+#     result=[]
+#     stack=[]
+#     curr=root
+#     while curr or stack:
+#         while curr:
+#             stack.append(curr)
+#             curr=curr.left
+#         curr=stack.pop()
+#         result.append(curr.val)
+#         curr=curr.right
+#     return result
+
+
+def inorder_recursive(root):
+    result=[]
+    def transverse(node):
+        if not node:
+            return
+        transverse(node.left)
+        result.append(node.val)
+        transverse(node.right)
+    transverse(root)
+    return result
+
+
+
+
+def postorder_recursive(root):
+    result=[]
+    def transverse(node):
+        if not node:
+            return
+        transverse(node.left)
+        transverse(node.right)
+        result.append(node.val)
+    transverse(root)
+    return result
+
+
+
+
+
+
+
+
 
